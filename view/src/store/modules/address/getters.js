0b73e00627fe7ea009a642cd4fa4f08c6013f896
tolamson2811 <100157005+tolamson2811@users.noreply.github.com>
2023-12-12 10:43:29 +0700

    feat: call api province

---
+++ view/src/store/modules/address/getters.js
@@ -0,0 +1,14 @@
+export default {
+    getProvinces(state) {
+        return state.provinces;
+    },
+
+    getDistricts(state) {
+        return state.districts;
+    },
+
+    getWards(state) {
+        return state.wards;
+    }
+
+}
---
+++ view/src/store/modules/address/mutations.js
@@ -0,0 +1,13 @@
+export default {
+    setProvinces(state, payload) {
+        state.provinces = payload;
+    },
+
+    setDistricts(state, payload) {
+        state.districts = payload;
+    },
+
+    setWards(state, payload) {
+        state.wards = payload;
+    }
+}
